68111de1280806bd4ab753d0be66204563349343
anthonyophernandez <anthonyophernandez@gmail.com>
2020-07-13 08:59:07 +0200

    Tailwind CSS installation

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-gray-300 w-full h-screen">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+html, body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
